1ff188c2ddc546e097b6520c2870420db2ac9688
TrevorPatorno <89477968+TrevorPatorno@users.noreply.github.com>
2024-03-18 20:26:29 -0500

    Update ExpressAuthentication.js; getting basics for assignment

--- ExpressAuthentication.js
+++ ExpressAuthentication.js
@@ -1,1 +1,22 @@
+const { MongoClient } = require("mongodb");
+const cookieParser = require("cookie-parser");
 
+// connection string for mongo db
+const uri = "mongodb+srv://expressAuthAccount:authenticator@cluster0.39hko4i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// standard stuff to get app working on browser
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+console.log('Server started at http://localhost:' + port);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+// Default route: should go to Register Page
+app.get('/', function(req, res) {
+  res.sendFile(__placeholder + '/register.html');
+});
